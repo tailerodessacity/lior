d0ddb1357918f2320f82748aab56f4ee6e226027
Robert <tailerodessacity@gmail.com>
2023-12-24 16:42:55 +0200

    Added feature to test PostController endpoints

--- tests/Feature/PostsControllerTest.php
+++ tests/Feature/PostsControllerTest.php
@@ -2,13 +2,16 @@
 
 namespace Tests\Feature;
 
+use App\Enums\PermissionComments;
+use App\Enums\PermissionPost;
+use App\Enums\Roles;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 use Tests\TestCase;
-use Database\Factories\PostsFactory;
-use Database\Factories\CommentsFactory;
 
 class PostsControllerTest extends TestCase
 {
@@ -17,55 +20,54 @@ class PostsControllerTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->seed();
     }
 
-//    public function testIndex()
-//    {
-//        $posts = Post::factory(10)->create();
-//
-//        $response = $this->get('/api/posts');
-//
-//        $response->assertStatus(200)
-//            ->assertJsonCount(10, 'data')
-//            ->assertJsonStructure([
-//                'data' => [
-//                    '*' => [
-//                        'id',
-//                        'title',
-//                        'slug',
-//                        'preview',
-//                        'detail',
-//                    ],
-//                ],
-//            ]);
-//    }
-//
-//    public function testShow()
-//    {
-//        $post = Post::factory()->create();
-//
-//        $user = User::factory()->create();
-//        $token = $this->getTokenForUser($user);
-//
-//        $response = $this->withHeaders([
-//            'Authorization' => 'Bearer ' . $token,
-//        ])->get('/api/posts/' . $post->id);
-//
-//        $response->assertStatus(200)
-//            ->assertJson([
-//                'id' => $post->id,
-//                'title' => $post->title,
-//                'slug' => $post->slug,
-//                'preview' => $post->preview,
-//                'detail' => $post->detail,
-//            ]);
-//    }
+    public function testIndex()
+    {
+        $this->createUserAndRoles();
+
+        Post::factory(10)->create();
+
+        $response = $this->get('/api/posts');
+
+        $response->assertStatus(200)
+            ->assertJsonCount(10, 'data')
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'title',
+                        'slug',
+                        'preview',
+                        'detail',
+                    ],
+                ],
+            ]);
+    }
+
+    public function testShow()
+    {
+        $token = $this->getTokenForUser();
+
+        $post = Post::factory()->create();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->get('/api/posts/' . $post->id);
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'id' => $post->id,
+                'title' => $post->title,
+                'slug' => $post->slug,
+                'preview' => $post->preview,
+                'detail' => $post->detail,
+            ]);
+    }
 
     public function testStore()
     {
-        $user = User::factory()->create();
-        $token = $this->getTokenForUser($user);
+        $token = $this->getTokenForUser();
 
         $postData = [
             'title' => 'Test Title',
@@ -88,16 +90,41 @@ class PostsControllerTest extends TestCase
 
     }
 
-    protected function getTokenForUser(User $user)
+    protected function getTokenForUser()
     {
+        $admin = $this->createUserAndRoles();
+
         $response = $this->post('/api/auth/login', [
-            'email' => $user->email,
+            'email' => $admin->email,
             'password' => 'HAS123456',
         ]);
 
         $token = $response->json('access_token');
 
         return $token;
+    }
+
+    protected function createUserAndRoles()
+    {
+        $admin = User::factory()->create([
+            'id' => 1,
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('HAS123456')
+        ]);
+
+        $adminRole = Role::create(['name' => Roles::ADMIN]);
+
+        $permissions = array_merge(PermissionComments::cases(), PermissionPost::cases());
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $adminRole->syncPermissions($permissions);
+        $admin->assignRole('admin');
+
+        return $admin;
     }
 }
 
