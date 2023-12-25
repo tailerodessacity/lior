c4c0725231011b17e2dc286cbd144f71f051e287
Robert <tailerodessacity@gmail.com>
2023-12-25 21:09:39 +0200

    Added email notification

---
+++ app/Notifications/AddNewComment.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Mail\AddNewCommentMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class AddNewComment extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        $this->onConnection('database');
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage())
+                    ->greeting('Hello!')
+                    ->line('A new comment has been added to the post');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Determine which queues should be used for each notification channel.
+     *
+     * @return array<string, string>
+     */
+    public function viaQueues(): array
+    {
+        return [
+            'mail' => 'notification_comments',
+        ];
+    }
+}
